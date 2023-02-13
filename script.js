bb3429bd2038c1911a07ecd626dbf6005d05cd27
mcanam910@gmail.com <mcanam910@gmail.com>
2023-02-13 13:56:29 +0700

    add Lenis library for smooth scrolling effect

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-(function() { 'use strict';
-
-      function featherIcons() {
-            const url = 'https://cdn.jsdelivr.net/npm/feather-icons@4.29.0/dist/icons/';
-            const $elems = document.querySelectorAll('i[data-feather]');
-
-            $elems.forEach($elem => {
-                  const name = $elem.dataset.feather;
-                  const icon = fetch(url + name + '.svg').then(res => res.text());
-                  
-                  icon.then(svg => {
-                        $elem.insertAdjacentHTML('beforebegin', svg);
-                        $elem.remove();
-                  });
-
-                  icon.catch(err => {
-                        console.warn(err.message);
-                  });
-            });
-      }
-
-      featherIcons();
-
+(function() { "use strict";
+    
+    function initLenis() {
+        const lenis = new Lenis({
+            duration: 2,
+            smooth: true,
+            mouseMultiplier: 1,
+            smoothTouch: true,
+            touchMultiplier: 4,
+        });
+        
+        function raf(time) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
+        
+        requestAnimationFrame(raf)
+    }
+    
+    initLenis();
+    
 })();
