3ff8100bf856b149fe001f2922deb5f489235269
mcanam <mcanam910@gmail.com>
2023-02-14 06:56:01 +0700

    add minimap view for desktop mode

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-(function() { "use strict";
-    
-    function initLenis() {
-        const lenis = new Lenis({
-            duration: 2,
-            smooth: true,
-            mouseMultiplier: 1,
-            smoothTouch: true,
-            touchMultiplier: 4,
-        });
-        
-        function raf(time) {
-            lenis.raf(time)
+(function () { "use strict";
+
+      function initLenis() {
+            const lenis = new Lenis({
+                  duration: 2,
+                  smooth: true,
+                  mouseMultiplier: 1,
+                  smoothTouch: true,
+                  touchMultiplier: 4,
+            });
+
+            function raf(time) {
+                  lenis.raf(time)
+                  requestAnimationFrame(raf)
+            }
+
             requestAnimationFrame(raf)
-        }
-        
-        requestAnimationFrame(raf)
-    }
-    
-    initLenis();
-    
+      }
+
+      function initMinimap() {
+            const $wrapper = document.querySelector(".wrapper");
+            const $content = document.querySelector(".minimap__content");
+            const $outline = document.querySelector(".minimap__outline");
+
+            const cloned = $wrapper.cloneNode(true);
+
+            const resize = () => {
+                  $content.style.width  = $wrapper.offsetWidth  + "px";
+                  $content.style.height = $wrapper.offsetHeight + "px";
+            }
+            
+            document.body.onscroll = () => {
+                  const top = document.documentElement.scrollTop;
+                  $outline.style.transform = `translateY(${top + "px"})`;
+            };
+
+            window.onresize = resize; resize();
+            $content.appendChild(cloned);
+      }
+
+      initLenis();
+      initMinimap();
+
 })();
